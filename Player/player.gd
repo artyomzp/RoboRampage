fa4b3c558d324e80242fc1469804f34029c8140b
Artem <serdyukartyom.zp@gmail.com>
2026-05-09 15:20:34 +0300

    improve sandbox

--- Player/player.gd
+++ Player/player.gd
@@ -2,9 +2,11 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+
+@export var jump_height: float = 1.0
 
 var mouse_motion := Vector2.ZERO
+var gravity : float
 
 @onready var camera_pivot: Node3D = $CameraPivot
 
@@ -12,14 +14,18 @@ func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta: float) -> void:
+	gravity = get_gravity().length()
 	handle_camera_rotation()
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		if velocity.y >= 0:
+			velocity.y -= gravity * delta
+		else:
+			velocity.y -= gravity * delta * 2.0
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = sqrt(jump_height * 2 * gravity)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
