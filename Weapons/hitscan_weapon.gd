88e141ad4f29e6a81631662dcd12252039960390
Artem <serdyukartyom.zp@gmail.com>
2026-05-10 18:55:51 +0300

    add raycast3d to the weapon

--- Weapons/hitscan_weapon.gd
+++ Weapons/hitscan_weapon.gd
@@ -1,8 +1,12 @@
 extends Node3D
 
 @export var fire_rate := 14.0
+@export var recoil := 0.05
+@export var weapon_mesh: Node3D
 
 @onready var cooldown_timer: Timer = $CooldownTimer
+@onready var weapon_position: Vector3 = weapon_mesh.position
+@onready var ray_cast_3d: RayCast3D = $RayCast3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,5 +17,11 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("fire"):
 		if cooldown_timer.is_stopped():
-			cooldown_timer.start(1.0 / fire_rate)
-			print("weapon fired!")
+			shoot()
+	weapon_mesh.position = weapon_mesh.position.lerp(weapon_position, delta * 10.0)
+			
+func shoot() -> void:
+	cooldown_timer.start(1.0 / fire_rate)
+	printt("weapon fired!", ray_cast_3d.get_collider())
+	weapon_mesh.position.z += recoil
+	
