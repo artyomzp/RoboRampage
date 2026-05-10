48d81cb4bf268160a489031dbd168a346d7ab01e
Artem <serdyukartyom.zp@gmail.com>
2026-05-10 15:02:53 +0300

    add enemy attack animation

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -3,36 +3,62 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const EPSILON = 0.001
+
+@export var attack_range: float = 1.5
 
 @onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 var player
 var gravity : float
+var provoked := false
+var agro_range := 12.6
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
 
+func _process(_delta: float) -> void:
+	if provoked:
+		navigation_agent_3d.target_position = player.global_position
+
 func _physics_process(delta: float) -> void:
 	gravity = get_gravity().length()
-	navigation_agent_3d.target_position = player.global_position
 	var next_postion = navigation_agent_3d.get_next_path_position()
 	
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		velocity.y -= gravity * delta
+	
+	var direction = global_position.direction_to(next_postion) 
+	var distance = global_position.distance_to(player.global_position)
+	
+	if  distance <= agro_range:
+		provoked = true
+		
+	if provoked:
+		if distance <= attack_range:
+			animation_player.play("Attack")
+	
 	if direction:
+		look_at_target(direction)
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
-#	move_and_slide()
+	move_and_slide()
+
+func look_at_target(direction: Vector3) -> void:
+	var adjusted_direction = direction
+	adjusted_direction.y = 0
+	if adjusted_direction.length_squared() > EPSILON: 
+		look_at(global_position + adjusted_direction, Vector3.UP, true)
+		
+func attack() -> void:
+	print("Enemy Attack!")
+	
+	
+	
+	
