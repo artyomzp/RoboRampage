6c0cec41bca636058d4e866f3735f63b45846a60
Artem <serdyukartyom.zp@gmail.com>
2026-05-15 08:02:18 +0300

    add basic rooms and corridors

--- Player/player.gd
+++ Player/player.gd
@@ -60,6 +60,9 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		if Input.is_action_pressed("aim"):
+			velocity.x *= aim_multiplier
+			velocity.z *= aim_multiplier
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
